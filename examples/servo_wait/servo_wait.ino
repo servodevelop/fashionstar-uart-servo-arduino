7087629145161a679a4005016a2677e9af307406
Shunkai Xing <xingshunkai@qq.com>
2021-06-02 15:32:59 +0800

    “完成等待例程”

--- examples/servo_wait/servo_wait.ino
+++ examples/servo_wait/servo_wait.ino
@@ -6,39 +6,46 @@
  * 邮箱: kyle.xing@fashionstar.com.hk
  * 更新时间: 2020/05/13
  */
-#include <SoftwareSerial.h>
 #include "FashionStar_UartServoProtocol.h"
 #include "FashionStar_UartServo.h" // Fashion Star串口总线舵机的依赖
 
-// 软串口的配置
+// 串口总线舵机配置参数
+#define SERVO_ID 0 //舵机ID号
+#define BAUDRATE 115200 // 波特率
+
+// 调试串口的配置
+#if defined(ARDUINO_ARCH_AVR)
+#include <SoftwareSerial.h>
 #define SOFT_SERIAL_RX 6
 #define SOFT_SERIAL_TX 7
-#define SOFT_SERIAL_BAUDRATE 4800
-
-// 配置参数
-#define BAUDRATE 115200 // 波特率
-#define SERVO_ID 0 //舵机ID号
-
 SoftwareSerial softSerial(SOFT_SERIAL_RX, SOFT_SERIAL_TX); // 创建软串口
+#define DEBUG_SERIAL softSerial
+#define DEBUG_SERIAL_BAUDRATE 4800
+#elif defined(ARDUINO_ARCH_ESP32)
+#define DEBUG_SERIAL Serial
+#define DEBUG_SERIAL_BAUDRATE 115200
+#endif 
+
 FSUS_Protocol protocol(BAUDRATE); //协议
 FSUS_Servo uservo(SERVO_ID, &protocol); // 创建舵机
 
 void setup(){
-    softSerial.begin(SOFT_SERIAL_BAUDRATE); // 初始化软串口的波特率
+    
     protocol.init(); // 通信协议初始化
     uservo.init(); //舵机初始化
-
-    softSerial.println("Test Wait");
+    // 打印例程信息
+    DEBUG_SERIAL.begin(DEBUG_SERIAL_BAUDRATE);
+    DEBUG_SERIAL.println("Test Wait");
 }
 
 void loop(){
-    softSerial.println("Set Angle = 90.0");
+    DEBUG_SERIAL.println("Set Angle = 90.0");
     uservo.setAngle(90.0); // 设置舵机的角度
     uservo.wait();
-    softSerial.println("Real Angle = "+String(uservo.curAngle, 2));
+    DEBUG_SERIAL.println("Real Angle = "+String(uservo.curAngle, 2));
      
-    softSerial.println("Set Angle = -90.0");
+    DEBUG_SERIAL.println("Set Angle = -90.0");
     uservo.setAngle(-90);
     uservo.wait();
-    softSerial.println("Real Angle = "+String(uservo.curAngle, 2));
+    DEBUG_SERIAL.println("Real Angle = "+String(uservo.curAngle, 2));
 }
